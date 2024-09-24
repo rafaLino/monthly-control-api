de705ad2b734014d76b8de1aebf3c03a91907558
Rafa Lino <rafael.linosp@gmail.com>
2024-09-23 23:04:19 -0300

    fe

--- src/handlers/extract.mjs
+++ src/handlers/extract.mjs
@@ -1,30 +1,30 @@
-import { extract } from '../functions/extract.mjs';
+import { extract } from "../functions/extract.mjs";
 
 export default async function (event) {
-    let body;
-    let statusCode = '200';
-    const headers = {
-        'Content-Type': 'application/json',
-    };
+  let body;
+  let statusCode = "200";
+  const headers = {
+    "Content-Type": "application/json",
+  };
 
-    try {
-        switch (event.httpMethod) {
-            case 'GET':
-                body = await extract();
-                break;
-            default:
-                throw new Error(`Unsupported method "${event.httpMethod}"`);
-        }
-    } catch (err) {
-        statusCode = '400';
-        body = err.message;
-    } finally {
-        body = JSON.stringify(body);
+  try {
+    switch (event.httpMethod) {
+      case "POST":
+        body = await extract();
+        break;
+      default:
+        throw new Error(`Unsupported method "${event.httpMethod}"`);
     }
+  } catch (err) {
+    statusCode = "400";
+    body = err.message;
+  } finally {
+    body = JSON.stringify(body);
+  }
 
-    return {
-        statusCode,
-        body,
-        headers,
-    };
+  return {
+    statusCode,
+    body,
+    headers,
+  };
 }
--- src/repository/bucket.mjs
+++ src/repository/bucket.mjs
@@ -1,19 +1,34 @@
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
 const ONE_HOUR = 60 * 60;
-const client = new S3Client({});
+const client = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+    bucket: process.env.BUCKET_NAME,
+  },
+});
+
+function getDate() {
+  const now = new Date();
+  const date = now.toISOString().split("-");
+  return `${date[0]}-${date[1]}`;
+}
 export async function saveAndGetLink(csv) {
   // Save CSV to S3
   const command = new PutObjectCommand({
     Bucket: process.env.BUCKET_NAME,
-    Key: "result.csv",
+
+    Key: getDate(),
     Body: csv,
     ContentType: "text/csv",
   });
 
   // Download the CSV file
-  const url = await getSignedUrl(client, command, { expiresIn: ONE_HOUR });
   await client.send(command);
+  const url = await getSignedUrl(client, command, { expiresIn: ONE_HOUR });
 
   return url;
 }
