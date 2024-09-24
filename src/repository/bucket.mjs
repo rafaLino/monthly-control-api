0964f02125cd9fa5a8fdc8da675d2efabeab28d0
Rafa Lino <rafael.linosp@gmail.com>
2024-09-24 00:12:01 -0300

    fix

--- src/repository/bucket.mjs
+++ src/repository/bucket.mjs
@@ -1,14 +1,9 @@
-import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { PutObjectCommand, GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 const ONE_HOUR = 60 * 60;
 const client = new S3Client({
   region: process.env.AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_KEY,
-    bucket: process.env.BUCKET_NAME,
-  },
 });
 
 function getDate() {
@@ -17,18 +12,34 @@ function getDate() {
   return `${date[0]}-${date[1]}`;
 }
 export async function saveAndGetLink(csv) {
+  const key = getDate();
   // Save CSV to S3
   const command = new PutObjectCommand({
     Bucket: process.env.BUCKET_NAME,
-
-    Key: getDate(),
+    Key: key,
     Body: csv,
     ContentType: "text/csv",
   });
+  await client.send(command);
 
   // Download the CSV file
-  await client.send(command);
+
+  return await getSignedLink(key);
+}
+
+async function getSignedLink(key) {
+  const command = new GetObjectCommand({
+    Bucket: process.env.BUCKET_NAME,
+    Key: key,
+  });
+
   const url = await getSignedUrl(client, command, { expiresIn: ONE_HOUR });
 
   return url;
+}
+
+function getDate() {
+  const now = new Date();
+  const date = now.toISOString().split("-");
+  return `${date[0]}-${date[1]}`;
 }
